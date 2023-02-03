f14d6d0e7cc223ee0425cc6789cb39db62f3c462
el-buku <56129429+el-buku@users.noreply.github.com>
2023-02-03 03:13:27 +0200

    Add ssh tmux support for Ctrl Shift Alt modifiers

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ mv $HOME/.temp.zshrc $HOME/.zshrc
 
 cd "$HOME/.oh-my-zsh/custom/plugins" && git clone https://github.com/MenkeTechnologies/zsh-expand.git 
 cd $HOME
+
+# Add ssh tmux support for Ctrl Shift Alt modifiers
+set -g terminal-overrides "xterm*:kLFT5=\eOD:kRIT5=\eOC:kUP5=\eOA:kDN5=\eOB:smkx@:rmkx@"
+
 source ./.zshrc
