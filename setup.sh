e66f2ea4568020d9fa7a95131ff49f612e3adeec
el-buku <56129429+el-buku@users.noreply.github.com>
2023-02-03 02:58:13 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd "$HOME/.oh-my-zsh/custom/plugins" && git clone https://github.com/MenkeTechnologies/zsh-expand.git 
+mv ./.zshrc $HOME/.zshrc
+mv ./.zsh_aliases $ HOME/.zsh_aliases
+mv ./.backup.zshrc $HOME/.backup.zshrc
+cd $HOME
+source ./.zshrc
